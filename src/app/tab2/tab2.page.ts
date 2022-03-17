7ca6c760a0c0de3a454dd55bca6125f90b4a78ef
Heppppe <mateusz.chlopecki1@gmail.com>
2022-03-17 12:39:48 +0100

    responsive chessboard

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,20 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class Tab2Page implements OnInit{
 
   ngOnInit(): void {
-    this.startPuzzles();
   }
   constructor() {}
-  async startPuzzles()
-  {
-    for(let i=0; i<64; i++)
-    {
-      const newFile = document.createElement("div");
-      newFile.className = "file";
-      ((i+Math.floor(i/8))%2 == 0)? newFile.style.background = "brown" : newFile.style.background = "white";
-      newFile.style.width = "30px";
-      newFile.style.height = "30px";
-      newFile.style.display = "inline-block";
-      document.querySelector("#board").appendChild(newFile)
-    }
-  }
 }
