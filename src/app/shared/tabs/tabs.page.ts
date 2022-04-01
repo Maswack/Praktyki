466266942710c4a68b26534b51d2260f52faee8f
Heppppe <mateusz.chlopecki1@gmail.com>
2022-04-01 14:48:25 +0200

    family guy

--- src/app/shared/tabs/tabs.page.ts
+++ src/app/shared/tabs/tabs.page.ts
@@ -10,4 +10,5 @@ export class TabsPage {
   constructor() {}
   @ViewChild('tab4', {read: ElementRef}) tab4: ElementRef;
   @ViewChild('tab3', {read: ElementRef}) tab3: ElementRef;
+  @ViewChild('zaslona', {read: ElementRef}) zaslona: ElementRef;
 }
